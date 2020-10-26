18782fd064408343913621b3c82e75b06bcdefe2
akashmony01 <akashmonya88@gmail.com>
2020-10-27 00:52:52 +0600

    first commit

---
+++ src/script/main.js
@@ -0,0 +1,11 @@
+// slider activation
+// -------------------------------------------------------
+jQuery(document).ready(function($) {
+	$('.owl-carousel').owlCarousel({
+	  center: true,
+	  dots: false,
+	  loop: true,
+	  margin: 50,
+	  autoWidth:true
+	});
+});
